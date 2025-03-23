649bfd94ded15ec65272aa90d81bb12f1b95d30f
Dimka <usenkoalekseh878@gmail.com>
2025-03-23 12:22:46 +0200

    feat: updated dockerfile.dev

--- src/app.py
+++ src/app.py
@@ -5,9 +5,5 @@ app = FastAPI()
 @app.get("/")
 async def main():
     return {
-        "message": "Hello world"
+        "message": "my-message"
     }
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, port=80, host='0.0.0.0', reload=True)
